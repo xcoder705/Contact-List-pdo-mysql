54e8532678cb9a5e355a1bc9629e23d221246868
xcoder705 <83127588+xcoder705@users.noreply.github.com>
2021-04-24 17:43:12 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 
-    </div> <!-- Comprovat -->
+    </div> 
 
     <!-- Link to Jquery -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
