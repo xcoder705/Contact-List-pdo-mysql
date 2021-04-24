70811bfa26738fbada1a3d6c009e1fcfd2c28999
xcoder705 <83127588+xcoder705@users.noreply.github.com>
2021-04-24 17:41:58 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+
+    </div> <!-- Comprovat -->
+
+    <!-- Link to Jquery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <!-- Link to Bootstrap Bundle + Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    
+  </body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<?php require_once("connexio_pdo_mysql.php"); ?> <!-- Comprovat -->
+
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Enllacem amb Bootstrap 5 -->
+    <!-- Link to Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style-agenda.css">
+    <title>Agenda PHP MYSQL</title>
+  </head>
+
+  <body>
+    
+    
+
+    
+       
